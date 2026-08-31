84994c2bcf4c8614daff5819be33589e389ba895
jaye311 <johnathanaye311@gmail.com>
2026-08-30 23:13:57 -0700

    Add support for IF statement for parser. Generated trees for given IF test case. Currently the TestIf1.pas test case does not pass, the negative sign for the number is identified by a syntax error. When inspected, it looks like parseSimpleExpression() should already cover this.

---
+++ Simple/TestIf2.pas
@@ -0,0 +1,9 @@
+program TestIf1;
+
+begin
+  i := 1;
+  j := 1;
+  IF (i = j) 
+    THEN t := 200
+  ELSE f := 200; 
+end.
--- Simple/src/frontend/Parser.java
+++ Simple/src/frontend/Parser.java
@@ -92,6 +92,7 @@ public class Parser
         statementStarters.add(IDENTIFIER);
         statementStarters.add(REPEAT);
         statementStarters.add(WHILE);
+        statementStarters.add(Token.TokenType.IF);
         statementStarters.add(DO);
         statementStarters.add(Token.TokenType.WRITE);
         statementStarters.add(Token.TokenType.WRITELN);
@@ -128,6 +129,7 @@ public class Parser
             case BEGIN :      stmtNode = parseCompoundStatement();   break;
             case REPEAT :     stmtNode = parseRepeatStatement();     break;
             case WHILE :      stmtNode = parseWhileStatement();      break;
+            case IF :         stmtNode = parseIfStatement();         break;
             case WRITE :      stmtNode = parseWriteStatement();      break;
             case WRITELN :    stmtNode = parseWritelnStatement();    break;
             case SEMICOLON :  stmtNode = null; break;  // empty statement
@@ -281,6 +283,40 @@ public class Parser
         if (stmtNode != null) loopNode.adopt(stmtNode);
 
         return loopNode;
+    }
+
+    private Node parseIfStatement()
+    {
+        //The current Node should now be IF.
+
+        //Create an IF node
+        Node ifNode = new Node(Node.NodeType.IF);
+
+        currentToken = scanner.nextToken(); //Consume IF
+
+        // The IF node adopts the EQ node as its first child
+        ifNode.adopt(parseExpression());
+
+        if (currentToken.type == THEN)
+        {
+            currentToken = scanner.nextToken(); // Consume THEN
+        }
+        else syntaxError("Expecting THEN");
+
+        // The IF node adopts the THEN node as its second child
+        Node thenNode = parseStatement();
+        if (thenNode != null) ifNode.adopt(thenNode);
+
+        if (currentToken.type == ELSE)
+        {
+            currentToken = scanner.nextToken(); // Consume ELSE
+            // The IF node adopts the ELSE node as its third child
+            Node elseNode = parseStatement();
+            if (elseNode != null) ifNode.adopt(elseNode);
+
+        }
+
+        return ifNode;
     }
     
     private Node parseWriteStatement()
